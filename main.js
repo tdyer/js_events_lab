b3b6fb9bd3eca6e1f4b9f34fd43485ba38ca3ebb
Tom Dyer <tdyer1@gmail.com>
2013-10-31 21:40:20 -0400

    Add handlers for mouse up/down and dblclick

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ var EventLab = EventLab || {};
 
 EventLab.button1 = document.getElementById('button1');
 
-//EventLab.button1.addEventListener('click', function(){alert('You clicked the button');});
-
-
+//EventLab.button1.addEventListener('click', function(){
+// 	alert('You clicked the button');
+// });
 
 // register button1 click handler.
-// EventLab.registerEventHandler(
-registerEventHandler(button1, "click", function(event){showEvent(event);});	
+registerEventHandler(button1, "click", showEvent);	
+registerEventHandler(button1, "dblclick", showEvent);
+registerEventHandler(button1, "mousedown", showEvent);
+registerEventHandler(button1, "mouseup", showEvent);
 
